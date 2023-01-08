b37797778ab43b7fcbe80cf4af5c18e9f66cbd90
marko-miletic <marko.g.miletic@gmail.com>
2023-01-08 13:04:29 +0100

    feat: update index template

--- src/routes/index.py
+++ src/routes/index.py
@@ -2,6 +2,8 @@ from flask import Blueprint, render_template, flash, redirect, url_for
 from flask_login import login_required, current_user
 
 from src.logs import logger
+from src.crud.discount_operations import get_current_discount_data
+from src.crud.mode_operations import get_modes_by_active_status
 from src.path_structure import TEMPLATES_DIRECTORY_PATH
 
 
@@ -10,4 +12,12 @@ index = Blueprint('index', __name__, template_folder=TEMPLATES_DIRECTORY_PATH)
 
 @index.route('/')
 def main():
-    return render_template('index.html')
+    try:
+        discount_data = get_current_discount_data()
+        active_washing_modes = get_modes_by_active_status(active_status=True)
+        return render_template('index.html',
+                               discount_data=discount_data,
+                               active_modes=active_washing_modes)
+    except Exception as err:
+        logger.logging.error(err)
+        return render_template('index.html')
